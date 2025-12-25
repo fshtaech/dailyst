251b20af98e0b7d422eeef5fe938e8312a98493a
fshtaech <caetachyon@protonmail.com>
2025-12-25 13:23:49 +0800

    Remove functions, migrate to another site, migrate to pnpm

--- src/db/firebase/firebase.config.ts
+++ src/db/firebase/firebase.config.ts
@@ -1,8 +1,4 @@
 import {
-  initializeApp as initFirebaseAdminApp,
-  type App,
-} from "firebase-admin/app";
-import {
   initializeApp as initFirebaseApp,
   type FirebaseApp,
 } from "firebase/app";
@@ -19,7 +15,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
-export const adminApp: App = initFirebaseAdminApp(firebaseConfig);
 export const firebaseApp: FirebaseApp = initFirebaseApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
 export const gAuth = new GoogleAuthProvider();
