b7055a464841607584c2cf276c0bf5677a27809c
fshtaech <caetachyon@protonmail.com>
2025-12-27 06:13:33 +0800

    Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,5 @@ import { createRoot } from "react-dom/client";
 import "./icons.css";
 import "./index.css";
 import { App } from "./App.tsx";
-import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
